eaf9b9ce95876281e01708665867463aec8463f0
John Bouyer <109704002+Turfader@users.noreply.github.com>
2025-05-23 10:25:04 -0700

    Added KIZ and oasis coords

---
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/teamAtlas/Helpers.java
@@ -0,0 +1,21 @@
+package jp.jaxa.iss.kibo.rpc.teamAtlas;
+
+class Constants
+{
+    // Keep In Zones
+    // [x_min, y_min, z_min], [x_max, y_max, z_max]
+    final double[][] KIZ1 = {{10.3, -10.2, 4.32}, {11.5, -6.0, 5.57}};
+    final double[][] KIZ2 = {{9.5, -10.5, 4.02}, {10.5, -9.6, 4.8}};
+
+    // Oasis zones
+    // [x_min, y_min, z_min], [x_max, y_max, z_max]
+    final double[][] oasis1 = {{10.425, -10.2, 4.445}, {11.425, -9.5, 4.945}};
+    final double[][] oasis2 = {{10.925, -9.5, 4.945}, {11.425, -8.45, 5.445}};
+    final double[][] oasis3 = {{10.425, -8.45, 4.945}, {10.975, -7.4, 5.445}};
+    final double[][] oasis4 = {{10.925, -7.4, 4.425}, {11.425, -6.35, 4.945}};
+}
+
+class Helpers
+{
+
+}
